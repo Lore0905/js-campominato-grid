8f2a23d5f929de7005ee4337a4129861a210e523
Lore0905 <lore.castelli4@gmail.com>
2021-11-24 15:05:30 +0100

    modifiche DOPO CORREZIONE

--- js/script.js
+++ js/script.js
@@ -1,32 +1,42 @@
+// quando l'utente clicca su play
+// h2 sparisce e compare la griglia
+// verifico cosa ha scelto l'utente e memorizzo la sua scelta 
+// creo x celle in base ai dati inseriti dall'utente
+// popolo le celle con il numero in ordine crescente
+// al click di ogni cella, si deve colorare lo sfondo di blue
+////////////////////////////////////////////////////////////////////////////////////////
 
-// PASSO 1
-// crare un arrey di numeri consecutivi.
+// seleziono il pulsante play
+const playButton = document.getElementById('play');
+playButton.addEventListener('click', startGame);
 
-const numberOfSquare = 49;
-let prova = consecutiveNumbersSquare(numberOfSquare);
-console.log(prova);
+// funzione click play-button
+function startGame(){
 
-// creo la funzione che mi permette di creare una quantità di numeri stabilita dalla let numberOfSquare che non sono ripetuti
-function consecutiveNumbersSquare (quantityOfNumbers) {
-    const totalnumbersArrey = [];
+    // rimuovo il text-main al click di play-button
+    const textMain = document.getElementById('text-main');
+    textMain.classList.add('hidden');
 
-    while(totalnumbersArrey.lenght < quantityOfNumbers) {
-        // genero un numero consecutivo
-        
-        let consecutivNumber = 0;
-        consecutivNumber++;
+    // aggiungo la griglia al click di play-button
+    const maingrid = document.getElementById('grid');
+    maingrid.classList.remove('hidden');
 
-        totalnumbersArrey.push(consecutivNumber);
-        
+    // memorizzo i dati nella select 
+    const selectValue = document.getElementById('select-level').value;
+
+    // seleziono la classe 'square' che mi ridimensiona le righe e colonne in base alla difficoltà scelta dll'utente
+    const selectSquare = document.getElementsByClassName("square");
+    console.log(selectSquare);
+
+    // creo la tabella in base al valore della select
+    if( selectValue === 'd-2' ) {
+        selectSquare.classList.add("d-medium");
     } 
-    return totalnumbersArrey;
+    else if( selectValue === 'd-3' ) {
+        selectSquare.classList.add("d-difficult");
+    }
+    console.log(selectValue);
 }
-
-
-
-
-
-
 
 
 
